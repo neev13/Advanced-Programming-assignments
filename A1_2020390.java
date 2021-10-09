2d52c6b568f4b7c249fc9ec09cf992f2a4a70703
neev13 <88670282+neev13@users.noreply.github.com>
2021-10-10 01:12:22 +0530

    Add files via upload

---
+++ A1_2020390.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+class Main_menu{
+    public static void main(String[]args){
+        System.out.println("CoWin Portal initialised....");
+        System.out.println("--------------------------------------\n1. Add Vaccine\n2. Register Hospital\n3. Register Citizen\n4. Add Slot for Vaccination\n5. Book Slot for Vaccination\n6. List all slots for a hospital\n7. Check Vaccination Status\n8. Exit");
+        System.out.println("--------------------------------------");
+
+    }
+}
